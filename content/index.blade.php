04666854868918e602b77161f714dc7980e26010
Eric Van Johnson <eric@diegodev.com>
2016-07-02 21:58:27 -0700

    Adding Episode 17

--- content/index.blade.php
+++ content/index.blade.php
@@ -10,6 +10,7 @@
         *recorded June 30th, 2016*
         @endmarkdown
 
+        <iframe width="100%" height="150" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/271940072&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
         @markdown
 
         [Sound Cloud](https://soundcloud.com/phpugly/episode17)
