eebf23e7e23eebdb459f5be447f01175218e66d3
Eric Van Johnson <eric@diegodev.com>
2016-08-01 07:18:44 -0700

    Moving shows to index page

--- content/index.blade.php
+++ content/index.blade.php
@@ -37,6 +37,148 @@
 
     </p>
     <hr>
+    <p class="section-paragraph">
+        @markdown
+        ### PHPUgly - Episode 20
+        *recorded July 21st, 2016*
+        @endmarkdown
+
+        <iframe width="100%" height="150" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/275225667&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
+
+        @markdown
+        [Sound Cloud](https://soundcloud.com/phpugly/episode20) | [Video](https://www.youtube.com/watch?v=GAwoYw8OatM)
+
+        **Topics**
+
+        * [SDPHP Downtown Meetup](http://www.meetup.com/sandiegophp/)
+        * [Some changes that are coming to Laravel 5.3](https://laravel-news.com/2016/07/laravel-5-3-changes-app-folder/)
+        * [How one Regex string brought down Stackoverflow](http://stackstatus.net/post/147710624694/outage-postmortem-july-20-2016)
+        * [PHP 7.1 hits Beta](http://php.net/index.php#id2016-07-21-1)
+        * [How Facebook can decide to revoke your license to React.js](http://react-etc.net/entry/your-license-to-use-react-js-can-be-revoked-if-you-compete-with-facebook)
+
+        On a side note, we would like to welcome the newest member of the PHPUgly family, Nolan Congdon, new son of the host [ John  Congdon ](https://twitter.com/johncongdon). All our best wishes go out to the entire Congdon family.
+
+
+        ## The hosts
+        * Eric Van Johnson [Twitter](https://twitter.com/shocm) / [Github](https://github.com/ericvanjohnson/) / [Blog](https://www.shocm.com) / [About.me](https://about.me/shocm)
+        * Tom Rideout [Twitter](https://twitter.com/realrideout) / [Github](https://github.com/trideout/) / [About.me](https://about.me/thomasrideout)
+        * John Congdon [Twitter](https://twitter.com/johncongdon) / [Github](https://github.com/johncongdon)
+
+        Follow us on Twitter [@PHPUgly](https://twitter.com/phpugly)
+
+        Email us at [Podacast@phpugly.com](mailto:podcast@phpugly.com)
+        @endmarkdown
+
+    </p>
+    <hr>
+
+    <p class="section-paragraph">
+        @markdown
+        ### PHPUgly - Episode 19
+        *recorded July 14th, 2016*
+        @endmarkdown
+        <iframe width="100%" height="150" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/274168460&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
+
+        @markdown
+        [Sound Cloud](https://soundcloud.com/phpugly/episode19) | [Uncut Video](https://www.youtube.com/watch?v=FH2AaP0zSHw)
+        **Topics**
+
+        * [PHP 5.5 Reaches EOL](http://php.net/supported-versions.php)
+        * [SDPHP Downtown Meetup](http://www.meetup.com/sandiegophp/)
+        * The Security Vulnerability of [target=_blank](https://medium.com/@jitbit/target-blank-the-most-underestimated-vulnerability-ever-96e328301f4c#.vfb3qummf)
+        * Many projects that use the [bad practice of extract ($_GET)](https://www.reddit.com/r/PHP/comments/4rtrrb/nearly_900000_projects_on_github_use_extract_get/)
+        * PHPStorm Plugin [Ideavim](https://plugins.jetbrains.com/plugin/164) gets an update
+        * [Github gets syntax highlighting for Blade Templates](https://laravel-news.com/2016/07/github-now-supports-blade-syntax-highlighting/)
+        * [Amazon's AWS buys Cloud9 Cloud IDE](https://techcrunch.com/2016/07/14/amazons-aws-buys-cloud9-to-add-more-development-tools-to-its-web-services-stack/)
+        * New service for reading PHP Internals called [Externals.io](http://externals.io/)
+        * Running your [RaspberryPi with PHP](https://www.sitepoint.com/powering-raspberry-pi-projects-with-php/)
+
+
+        ## The hosts
+        * Eric Van Johnson [Twitter](https://twitter.com/shocm) / [Github](https://github.com/ericvanjohnson/) / [Blog](https://www.shocm.com) / [About.me](https://about.me/shocm)
+        * Tom Rideout [Twitter](https://twitter.com/realrideout) / [Github](https://github.com/trideout/) / [About.me](https://about.me/thomasrideout)
+        * John Congdon [Twitter](https://twitter.com/johncongdon) / [Github](https://github.com/johncongdon)
+
+        Follow us on Twitter [@PHPUgly](https://twitter.com/phpugly)
+
+        Email us at [Podacast@phpugly.com](mailto:podcast@phpugly.com)
+        @endmarkdown
+
+    </p>
+    <hr>
+
+    <p class="section-paragraph">
+        @markdown
+        ### PHPUgly - Episode 18:What Time Is It?
+        *recorded July 8th, 2016*
+        @endmarkdown
+
+        <iframe width="100%" height="150" scrolling="no" frameborder="no"
+                src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/272948036&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
+        @markdown
+        [Sound Cloud](https://soundcloud.com/phpugly/episode18) | [Uncut
+        Video](https://www.youtube.com/watch?v=LaQe9CC3oZg)
+
+        **Topics**
+
+        * A blog post called [The Autistic Programmer](http://shocm.me/29z2SRd)
+        * [Do PHP and IoT Have a Future Together?](https://www.sitepoint.com/do-php-and-iot-have-a-future-together/)
+        * Pipes (||) vs OR
+        * The joys of Timezones in code. How is the best way to handle them?
+        * Laracon.us now offering a [live streaming
+        package](https://www.picatic.com/laracon-us-2016?code=dat-live-stream)
+        * Laravel Package [Mailthief](https://github.com/tightenco/mailthief)
+        * A peak at [VueJS 2.0](https://dotdev.co/peeking-into-vue-js-2-part-1-b457e60c88c6#.cgxxbjbef) and what to
+        expect
+
+        ## The hosts
+        * Eric Van Johnson [Twitter](https://twitter.com/shocm) / [Github](https://github.com/ericvanjohnson/) /
+        [Blog](https://www.shocm.com) / [About.me](https://about.me/shocm)
+        * Tom Rideout [Twitter](https://twitter.com/realrideout) / [Github](https://github.com/trideout/) /
+        [About.me](https://about.me/thomasrideout)
+        * John Congdon [Twitter](https://twitter.com/johncongdon) / [Github](https://github.com/johncongdon)
+
+        Follow us on Twitter [@PHPUgly](https://twitter.com/phpugly)
+
+        Email us at [Podacast@phpugly.com](mailto:podcast@phpugly.com)
+        @endmarkdown
+
+    </p>
+    <hr>
+    <p class="section-paragraph">
+        @markdown
+        #### Episode 17:Trademark This
+        *recorded June 30th, 2016*
+        @endmarkdown
+
+        <iframe width="100%" height="150" scrolling="no" frameborder="no"
+                src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/271940072&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
+        @markdown
+
+        [Sound Cloud](https://soundcloud.com/phpugly/episode17) | [Uncut Video](https://youtu.be/Kd2rZ0EjDdQ)
+
+        **Topics**
+
+        * Let's Encrypt Trademark issues
+        * PHP Alpha 2 released
+        * [October CMS version 1](https://octobercms.com/) releasing July 2nd
+        * [Zend Framework 3.0 released](https://github.com/zendframework/zendframework/releases/tag/release-3.0.0)
+        * [EU's Crazy Cookie Laws](https://murze.be/2016/06/make-laravel-app-comply-crazy-eu-cookie-law/)
+
+        ## The hosts
+        * Eric Van Johnson [Twitter](https://twitter.com/shocm) / [Github](https://github.com/ericvanjohnson/) /
+        [Blog](https://www.shocm.com) / [About.me](https://about.me/shocm)
+        * Tom Rideout [Twitter](https://twitter.com/realrideout) / [Github](https://github.com/trideout/) /
+        [About.me](https://about.me/thomasrideout)
+        * John Congdon [Twitter](https://twitter.com/johncongdon) / [Github](https://github.com/johncongdon)
+
+        Follow us on Twitter [@PHPUgly](https://twitter.com/phpugly)
+
+        Email us at [Podacast@phpugly.com](mailto:podcast@phpugly.com)
+        @endmarkdown
+
+    </p>
+    <hr>
     <h2>Previous Shows</h2>
 
     @foreach($paginatedBlogPosts as $post)
