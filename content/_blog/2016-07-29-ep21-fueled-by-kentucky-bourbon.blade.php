7b8186a6ebffcf1c598bf4b8fe8c08ff02003bb8
Eric Van Johnson <eric@diegodev.com>
2016-08-01 07:23:32 -0700

    Fixing video link on latest show

--- content/_blog/2016-07-29-ep21-fueled-by-kentucky-bourbon.blade.php
+++ content/_blog/2016-07-29-ep21-fueled-by-kentucky-bourbon.blade.php
@@ -23,7 +23,7 @@
         <iframe width="100%" height="150" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/276287000&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
 
         @markdown
-        [Sound Cloud](https://soundcloud.com/phpugly/episode20) | [Video](https://www.youtube.com/watch?v=GAwoYw8OatM)
+        [Sound Cloud](https://soundcloud.com/phpugly/episode20) | [Video](https://www.youtube.com/watch?v=QfFPT7fUcPk)
 
         **Topics**
 
